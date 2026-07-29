a1828cb0a35c0bfcf693c2a1cd9f25b2fba1273b
aijanarated <janahani017@gmail.com>
2026-07-30 01:23:59 +0300

    Update world.js

--- world.js
+++ world.js
@@ -1,1 +1,37 @@
+function createBlock(x, y, z, color){
 
+    const block = new THREE.Mesh(
+        new THREE.BoxGeometry(1,1,1),
+        new THREE.MeshLambertMaterial({
+            color: color
+        })
+    );
+
+    block.position.set(
+        x,
+        y,
+        z
+    );
+
+    scene.add(block);
+}
+
+
+function createWorld(){
+
+    for(let x = -10; x <= 10; x++){
+
+        for(let z = -10; z <= 10; z++){
+
+            createBlock(
+                x,
+                0,
+                z,
+                0x55aa33
+            );
+
+        }
+
+    }
+
+}
