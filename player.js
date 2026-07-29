5ea9a45202747c67eae89df266c26a0bb393bd51
aijanarated <janahani017@gmail.com>
2026-07-30 01:41:48 +0300

    Update player.js

--- player.js
+++ player.js
@@ -1,1 +1,30 @@
+let controls;
 
+function createPlayer(){
+
+    controls = new THREE.PointerLockControls(
+        camera,
+        document.body
+    );
+
+
+    scene.add(
+        controls.getObject()
+    );
+
+
+    controls.getObject().position.set(
+        0,
+        2,
+        5
+    );
+
+
+    document.addEventListener(
+        "click",
+        function(){
+            controls.lock();
+        }
+    );
+
+}
